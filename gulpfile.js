1e5aaa91a5c13f415bd70d903b17250cdf8a9bd3
RED Academy <redacademy@REDs-MacBook-Pro.local>
2019-10-21 14:05:21 -0700

    starter kit

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,52 @@
 const gulp = require("gulp"),
   terser = require("gulp-terser"),
   rename = require("gulp-rename"),
-  browserSync = require("browser-sync");
-gulp.task("scripts", function() {
+  browserSync = require("browser-sync"),
+  eslint = require("gulp-eslint");
+
+
+gulp.task("lint", function(){
   return gulp
-    .src("./js/*.js")
-    .pipe(terser())
-    .pipe(rename({ extname: ".min.js" }))
-    .pipe(gulp.dest("./build/js"));
+      .src("./js/*.js")
+      .pipe(eslint())
+      .pipe(eslint.format())
+      .pipe(eslint.failAfterError());
 });
+
+gulp.task("scripts", 
+  gulp.series("lint",
+    function scripts() {
+      return gulp
+        .src("./js/*.js")
+        .pipe(terser())
+        .pipe(rename({ extname: ".min.js" }))
+        .pipe(gulp.dest("./build/js"));
+    }
+  )
+);
+
+
 gulp.task("say_hello", function(done) {
   console.log("Hello!");
   done();
 });
-gulp.task("watch", function() {
-    gulp.watch("js/*.js", gulp.series("scripts"));
-  });
-  gulp.task('browser-sync', function() {
-    browserSync.init({
-        server: {
-            baseDir: "./"
-        }
-    });
-  gulp.watch(["*.html", "build/js/*.js"])
-    .on("change", browserSync.reload);   
-});
 
-// or...
-
+/**
+ * Browser Sync
+ */
 gulp.task('browser-sync', function() {
-    browserSync.init({
-        proxy: "yourlocal.dev"
-    });
+  browserSync.init({
+      server: {
+          baseDir: "./"
+      }
+  });
+
+  gulp.watch(["*.html", "build/js/*.js", "css/*.css"])
+      .on("change", browserSync.reload);
 });
-gulp.task("default", gulp.parallel("browser-sync", "scripts"));
+
+gulp.task("watch", function() {
+  gulp.watch("js/*.js", gulp.series("scripts"));
+});
+
+gulp.task("default", gulp.parallel("browser-sync", "watch"));
